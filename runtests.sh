4510cb9159891096a7427a30947119da728a1498
Ben Holland <benjholla@gmail.com>
2016-01-12 14:48:45 -0600

    adding logic to run doop over each test case

--- runtests.sh
+++ runtests.sh
@@ -2,10 +2,22 @@
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd ${dir}/JPATS/testsuite
+# clear past results
+rm -r ${dir}/results
+mkdir -p ${dir}/results
 
+# run benchmarks
+cd ${dir}/JPATS/testsuite
 for test in *.jar
 do
+        jar=${dir}/JPATS/testsuite/$test
 	class=${test%.*}
-	java -cp ${test} tests.type.${class}
+
+        echo "Testing:" $class
+        java -cp ${jar} tests.type.${class} &> ${dir}/results/$class.key
+
+	while read strategy; do
+		cd ${dir}/doop/
+		./doop -a $strategy -j $jar -Xstats:none
+	done < ${dir}/strategies.txt
 done
