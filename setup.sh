43fbfc183e02f7b2803587184d2d52cc17bf5d30
Ben Holland <bholland@iastate.edu>
2016-01-12 13:35:55 -0600

    fixed setup script

--- setup.sh
+++ setup.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-# Sanity checks: is a logicblox tarball present?
-tarballs = Dir.glob("logicblox-*.tar.gz")
-if tarballs.length == 0 then
-    puts "No logicblox tarball found in current directory, please make sure you copy or hardlink a logicblox-*.tar.gz release tarball into the current directory."
-    puts "You can download release tarballs from https://download.logicblox.com"
-    Kernel.exit(1)
-elsif File.symlink?(tarballs[0]) then
-    puts "You cannot symlink a logicblox tarball, it has to be a copy or hardlink, you can create a hardlink using 'ln' without using the '-s' switch."
-    Kernel.exit(2)
-end
+# Setup DOOP requirements (LogicBlox)
+logicblox=${logicblox-*.tar.gz}
+version3=logicblox-3
 
-tar xzf /vagrant/logicblox-*.tar.gz
+if [ ${logicblox/$version3} = $logicblox ] ; then
+  echo "Found" $logicblox
+else
+  echo "A version of the LogicBlox 3 tarball must be placed in this directory."
+  echo "You can download release tarballs from https://download.logicblox.com"
+  exit 1
+fi
 
-echo "Configuring Doop..."
+echo "Extracting LogicBlox..."
+tar xzf $logicblox
+
+echo "Downloading DOOP..."
 hg clone https://bitbucket.org/yanniss/doop doop
-hg clone https://bitbucket.org/yanniss/doop-benchmarks doop-benchmarks
+
+echo "Downloading DOOP Tools..."
 hg clone https://bitbucket.org/yanniss/doop-tools doop-tools
 
-echo "Please run 'source environment.sh'"
+echo "Downloading DOOP Runtimes and Benchmarks"
+hg clone https://bitbucket.org/yanniss/doop-benchmarks doop-benchmarks
+
+echo "Finished setup."
+echo "Please run 'source environment.sh' to configure DOOP environment."
