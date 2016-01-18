6b6c72e996ccf0562792714cc41a0ebf141ff9c1
Ben Holland <benjholla@gmail.com>
2016-01-18 14:00:07 -0600

    combining some steps

--- mkjars.sh
+++ mkjars.sh
@@ -2,6 +2,10 @@
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# clear out old tests
+rm -rf ${dir}/JPATS/testsuite
+
+# build tests from source
 cd ${dir}/JPATS/src/tests/type/
 mkdir -p ${dir}/JPATS/testsuite/
 
--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+echo "Building tests cases..."
+./mkjars.sh
+
+echo "Analyzing test cases..."
+echo "Progress will be written to 'progress.txt', run 'tail -f progress.txt' to monitor progress."
 rm -f progress.txt
-./tests.sh &> progress.txt&
+./tests.sh &>> progress.txt&
