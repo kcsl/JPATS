f7e26f97f2b4aea8a231529923b87b4761dd3b14
Ben Holland <benjholla@gmail.com>
2016-01-19 11:43:58 -0600

    adding some more fixes for error cases

--- tests.sh
+++ tests.sh
@@ -21,8 +21,8 @@ while read strategy; do
                 java -cp ${jar} tests.type.${class} &> ${dir}/results/$class/runtime-result.txt
 
                 cd ${dir}/doop/
+                rm -f last-analysis
                 ./doop -a $strategy -j $jar -Xstats:none &> ${dir}/results/$class/$strategy.doop.log
                 time bloxbatch -db last-analysis -query '_(?type) <- MethodInvocation:Signature[?mInvocation]="<java.lang.Object: java.lang.Class getClass()>", VirtualMethodInvocation:Base[?mInvocation] = ?base, AssignReturnValue[?mInvocation] = ?to, ActualParam[0, ?newmInvocation] = ?to, MethodInvocation:Signature[?newmInvocation]="<java.io.PrintStream: void println(java.lang.Object)>", VarPointsTo(_, ?heap, _, ?base), HeapAllocation:Type[?heap]=?type.' &> ${dir}/results/$class/$strategy.result.txt
         done
-done < ${dir}/simple-strategies.txt
-
+done < ${dir}/single-strategy.txt
